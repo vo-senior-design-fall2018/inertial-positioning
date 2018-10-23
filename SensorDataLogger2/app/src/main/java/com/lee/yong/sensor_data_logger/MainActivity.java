d1aef541b4aae70ffa8a43c4d865af7545852e01
Yong Su Lee <dkrcjs7521@gmail.com>
2018-10-23 17:58:41 -0400

    Gravity Data

--- SensorDataLogger2/app/src/main/java/com/lee/yong/sensor_data_logger/MainActivity.java
+++ SensorDataLogger2/app/src/main/java/com/lee/yong/sensor_data_logger/MainActivity.java
@@ -95,11 +95,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if(isRunning) {
             try {
                 switch(evt.sensor.getType()) {
+                    case Sensor.TYPE_GRAVITY:
+                        writer.write(String.format("%d; GRAVITY; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], 0.f, 0.f, 0.f));
+                        break;
                     case Sensor.TYPE_ACCELEROMETER:
                         writer.write(String.format("%d; ACC; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], 0.f, 0.f, 0.f));
-                        break;
-                    case Sensor.TYPE_GYROSCOPE_UNCALIBRATED:
-                        writer.write(String.format("%d; GYRO_UN; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], evt.values[3], evt.values[4], evt.values[5]));
                         break;
                     case Sensor.TYPE_GYROSCOPE:
                         writer.write(String.format("%d; GYRO; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], 0.f, 0.f, 0.f));
@@ -107,14 +107,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     case Sensor.TYPE_MAGNETIC_FIELD:
                         writer.write(String.format("%d; MAG; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], 0.f, 0.f, 0.f));
                         break;
-                    case Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED:
-                        writer.write(String.format("%d; MAG_UN; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], 0.f, 0.f, 0.f));
-                        break;
                     case Sensor.TYPE_ROTATION_VECTOR:
                         writer.write(String.format("%d; ROT; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], evt.values[3], 0.f, 0.f));
-                        break;
-                    case Sensor.TYPE_GAME_ROTATION_VECTOR:
-                        writer.write(String.format("%d; GAME_ROT; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], evt.values[3], 0.f, 0.f));
                         break;
                 }
             } catch (IOException e) {
